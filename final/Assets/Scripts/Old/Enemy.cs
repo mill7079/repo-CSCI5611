a0eeaa31e05fdc450d7a11caf64bb3521fda7a58
Allison Miller <mill7079@umn.edu>
2020-05-14 00:01:29 -0500

    Final Project

---
+++ final/Assets/Scripts/Old/Enemy.cs
@@ -0,0 +1,198 @@
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class Enemy : MonoBehaviour
+//{
+//    GameObject player;
+//    PlayerController playerController;
+
+//    //public float move = 3.0f;
+//    //private int direction = 1;
+//    Rigidbody2D body;
+//    Animator animator;
+//    Vector2 lookDirection = new Vector2(1, 0);
+//    Vector2 origin;
+//    public bool pathCreated = false;
+
+//    // motion planning
+//    Point start, goal;
+//    List<Vector2> path;
+//    float dt = 0.009f;
+
+//    // rpg mechanics
+//    public float health, attack, defense;
+//    private float maxHealth;
+//    public float attackRadius;
+//    public int speed; // affects movement speed
+//    public GameObject rangedAttack; // if null, unit has no ranged attack; otherwise holds projectile
+//    private bool isDead = false;
+
+//    void Awake()
+//    {
+//        body = GetComponent<Rigidbody2D>();
+//        origin = body.position;
+//        animator = GetComponent<Animator>();
+//        path = new List<Vector2>();
+//        Physics2D.IgnoreLayerCollision(8, 10);
+
+//        Dungeon.AddNewEnemy(this);
+
+//        player = GameObject.FindGameObjectWithTag("Player");
+//        playerController = player.GetComponent<PlayerController>();
+
+//        maxHealth = health;
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        // attack player - don't move if close enough to attack
+//        Vector2 pos = body.position;
+//        Vector2 move = new Vector2(0, 0);
+//        bool goodPath = Dungeon.GoodPath(pos, player.gameObject.transform.position);
+
+//        if (Time.frameCount % 80 == 0 && goodPath && (player.gameObject.transform.position - this.gameObject.transform.position).magnitude <= attackRadius)
+//        {
+//            playerController.Damage(attack);
+//            return;
+//        }
+
+//        // motion planning with path smoothing
+
+//        if (goodPath)
+//        {
+//            move = (path[path.Count - 1] - pos).normalized * dt * speed;
+//            body.MovePosition(body.position + move);
+//        }
+//        else
+//        {
+//            for (int i = path.Count - 1; i >= 0; i--)
+//            {
+//                if (Dungeon.GoodPath(pos, path[i]))
+//                {
+//                    move = (path[i] - pos).normalized * dt * speed;
+//                    body.MovePosition(body.position + move);
+//                    break;
+//                }
+//            }
+//        }
+        
+//        //// animation
+//        //if (animator != null)
+//        //{
+//        //    float changeX = pos.x - body.position.x;
+//        //    float changeY = pos.y - body.position.y;
+
+//        //    if (changeX < 0) animator.SetFloat("LookX", -1);
+//        //    else animator.SetFloat("LookX", 1);
+
+//        //    if (changeY < 0) animator.SetFloat("LookY", -1);
+//        //    else animator.SetFloat("LookY", 1);
+//        //}
+
+//        if (animator != null)
+//        {
+//            if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+//            {
+//                lookDirection.Set(move.x, move.y);
+//                lookDirection.Normalize();
+//            }
+
+//            animator.SetFloat("LookX", lookDirection.x);
+//            animator.SetFloat("LookY", lookDirection.y);
+//        }
+//    }
+
+//    public Point GetPos() { return new Point(body.position); }
+
+//    //private void OnTriggerEnter2D(Collision2D collision)
+//    //{
+//    //    //direction *= -1;
+//    //    //if (collision.gameObject.CompareTag("Particle"))
+//    //    Particle p = collision.gameObject.GetComponent<Particle>();
+//    //    if (p != null)
+//    //    {
+//    //        Debug.Log("collision with enemy");
+//    //        health -= p.Hit();
+//    //    }
+//    //}
+
+
+//    // motion planning code
+//    public void UpdateEndpoints(Point s, Point g)
+//    {
+//        start = s;
+//        goal = g;
+//    }
+
+//    // backtrace from goal to create path
+//    public void CreatePath()
+//    {
+//        pathCreated = true;
+//        //path = new List<Vector2>();
+//        List<Vector2> newPath = new List<Vector2>();
+//        bool okPath = true;
+//        //Debug.Log("start: " + start.GetPos() + " end: " + goal.GetPos());
+//        if (start == goal) return;
+
+//        Point g = goal;
+//        Vector2 endPos = goal.GetPos();
+//        int x = 0;
+//        while (endPos != start.GetPos() && x < 1000)
+//        {
+//            //Debug.Log("endpos: "+endPos + " start: " + start.GetPos());
+//            //path.Insert(0, endPos);
+//            newPath.Insert(0, endPos);
+//            g = g.GetParent();
+//            try
+//            {
+//                endPos = g.GetPos();
+//            }
+//            catch (Exception)
+//            {
+//                // path to user does not exist - keep on original path
+//                okPath = false;
+//                break;
+//                //return;
+//            }
+//            x++;
+//        }
+//        //curPath = path.get(1).sub(path.get(0));
+
+//        //path.Insert(0, endPos);
+//        newPath.Insert(0, endPos);
+
+//        // only update path if there is a path
+//        if (okPath) path = newPath;
+//        //curPath = path[1] - body.position;
+//    }
+
+//    // creates simple path from a to b
+//    public void InitPath(Point a, Point b)
+//    {
+//        path.Add(a.GetPos());
+//        path.Add(b.GetPos());
+//    }
+
+//    // handles attack from player
+//    //public void Damage(int att)
+//    public void Damage(float att, bool particle)
+//    {
+//        if (particle) health -= att;
+//        else health -= (att - defense);
+
+//        if (health <= 0) isDead = true;
+//    }
+
+//    public bool IsDead()
+//    {
+//        return isDead;
+//    }
+
+//    public float GetCurrentHealth()
+//    {
+//        return health / maxHealth;
+//    }
+//}
---
+++ final/Assets/Scripts/Old/PlayerController.cs
@@ -0,0 +1,156 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class PlayerController : MonoBehaviour
+//{
+//    Rigidbody2D body;
+//    public float moveSpeed = 3.0f;
+
+//    Room currentRoom;
+//    Vector2 lookDirection = new Vector2(1, 0);
+
+//    Animator animator;
+
+//    // rpg mechanics
+//    public float health, attack, defense;
+//    public float attackRadius, attackAngle;
+//    private Vector2 attackDir;
+//    private bool isDead = false;
+
+//    public float detectRadius;
+//    private float maxHealth;
+
+//    Particles magic;
+
+//    void Awake()
+//    {
+//        //DontDestroyOnLoad(gameObject);
+//        body = GetComponent<Rigidbody2D>();
+
+//        animator = GetComponent<Animator>();
+//        Physics2D.IgnoreLayerCollision(8, 10);
+
+//        attackDir = lookDirection;
+
+//        magic = GetComponentInChildren<Particles>();
+
+//        maxHealth = health;
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        if (isDead)
+//        {
+//            //Debug.Log("you be dead");
+//            return;
+//        }
+//        /** moving/animating **/
+
+//        float horizontal = Input.GetAxis("Horizontal");
+//        float vertical = Input.GetAxis("Vertical");
+//        Vector2 move = new Vector2(horizontal, vertical);
+//        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+//        {
+//            lookDirection.Set(move.x, move.y);
+//            lookDirection.Normalize();
+//        }
+
+//        animator.SetFloat("Look X", lookDirection.x);
+//        animator.SetFloat("Look Y", lookDirection.y);
+
+//        Vector2 position = body.position;
+//        position = position + move * moveSpeed * Time.deltaTime;
+//        body.MovePosition(position);
+
+
+//        /** attacking **/
+
+//        // ensure player is always able to attack even if not moving
+//        if (!Mathf.Approximately(move.magnitude, 0.0f))
+//        {
+//            attackDir = move;
+//            attackDir.Normalize();
+//        }
+
+//        // fires selected physical attack
+//        if (Input.GetButtonDown("Fire1"))
+//        {
+//            // attack in direction player is facing
+//            Vector3 look = new Vector3(attackDir.x, attackDir.y, 0);
+
+//            // find enemies in range (layer 8 is units)
+//            Collider2D[] enemiesInRange = Physics2D.OverlapCircleAll(body.position, attackRadius, (1 << 8));
+//            //Debug.Log("found " + enemiesInRange.Length + " enemies in circular range");
+//            for (int i = 0; i < enemiesInRange.Length; i++)
+//            {
+//                if (!enemiesInRange[i].CompareTag("Enemy")) continue;
+
+//                Enemy e = enemiesInRange[i].GetComponent<Enemy>();
+//                Rigidbody2D eBody = e.GetComponent<Rigidbody2D>();
+//                Vector2 toEnemy = eBody.position - body.position;
+
+//                //Debug.Log("Angle: " + Vector2.Angle(look, toEnemy));
+//                if (Vector2.Angle(look, toEnemy) < attackAngle)
+//                {
+//                    // enemy is in range
+//                    e.Damage(attack, false);
+//                    //Debug.Log("attacked enemy");
+//                }
+//            }
+//        }
+
+//        // fires selected magical attack
+//        if (Input.GetButtonDown("Fire2"))
+//        {
+//            Debug.Log("fire magic");
+//            magic.Fire(attackDir);
+//        }
+
+//    }
+
+//    public Room GetCurrentRoom()
+//    {
+//        return currentRoom;
+//    }
+
+//    public Point GetPos() { return new Point(body.position); }
+
+//    public bool MoveTo(Room destination)
+//    {
+//        // TODO move to location of door
+//        if (destination == null) return false;
+//        currentRoom = destination;
+//        return true;
+//    }
+
+//    public void SetLocation(Vector2 pos)
+//    {
+//        //body.MovePosition(pos);
+//        GameObject player = GameObject.FindGameObjectWithTag("Player");
+//        player.transform.position = pos;
+//    }
+
+//    //public void Damage(int att)
+//    public void Damage(float att)
+//    {
+//        //Debug.Log("ouch");
+//        if (att - defense < 1) health -= 1;
+//        else health -= (att - defense);
+
+//        if (health <= 0) isDead = true;
+//    }
+
+//    public bool IsDead()
+//    {
+//        return isDead;
+//    }
+
+//    //private void OnGUI()
+//    //{
+//    //    Debug.Log("show gui");
+//    //    string showHealth = "" + health + "/" + maxHealth;
+//    //    GUI.Label(new Rect(body.position.x, body.position.y + 0.5f, 50, 10), showHealth);
+//    //}
+//}
